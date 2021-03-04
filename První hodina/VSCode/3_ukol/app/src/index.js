b3cdd1541132c71257c4161740e1dd0b4eb3a26a
TomasRacil <tomas.racil@unob.cz>
2021-03-04 11:02:17 +0100

    React in remote added

---
+++ První hodina/VSCode/3_ukol/app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element= <h1>Hello world from react something!</h1>;
+
+///console.log(element);
+
+ReactDOM.render(element,document.getElementById('root'));
