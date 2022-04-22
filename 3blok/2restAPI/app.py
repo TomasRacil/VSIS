017354ece24619932af7d2ae9c82f77bdfb9b7d4
TomasRacil <tomas.racil@unob.cz>
2022-04-22 08:39:26 +0200

    1.0.0 - request added, modified examples

--- 3blok/2restAPI/app.py
+++ 3blok/2restAPI/app.py
@@ -11,9 +11,11 @@ todos_json = []
 
 osoby = []
 
+
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'data': 'hello world'}
+
 
 class TodoSimple(Resource):
     def get(self, id):
@@ -27,28 +29,31 @@ class TodoSimple(Resource):
         todos[id] = request.form['data']
         return {id: todos[id]}
 
+
 class TodoJson(Resource):
     def get(self, id):
         return todos_json[id]
-    
+
     def post(self, id):
         todos_json.append(request.json)
-        id= len(todos_json)-1
-        return {id:todos_json[id]}
+        id = len(todos_json)-1
+        return {id: todos_json[id]}
 
 
 class Osoba(Resource):
     def post(self):
-        sent_data=request.json
+        sent_data = request.json
         osoby.append(sent_data)
         print(sent_data)
+
     def get(self):
         return osoby
+
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(TodoSimple, '/todo/<int:id>')
 api.add_resource(TodoJson, '/json/<int:id>')
-api.add_resource(Osoba,'/osoba')
+api.add_resource(Osoba, '/osoba')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 3blok/3connecting_to_database/main.py
+++ 3blok/3connecting_to_database/main.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:postgres@db:5432/test'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -15,18 +16,19 @@ class User(Base):
     username = Column(String(80), unique=True, nullable=True)
     local_part = Column(String(64),  nullable=False)
     domain = Column(String(255), nullable=False)
-    password = Column(String(60),nullable=False)
+    password = Column(String(60), nullable=False)
 
     def __repr__(self):
         return f"<User(username='{self.username}', email='{self.local_part}@{self.domain}', nickname='{self.password}')>"
 
-#User.__table__
-#Base.metadata.create_all(engine)
+
+User.__table__
+Base.metadata.create_all(engine)
 #ed_user = User(username='ed', local_part='ed', domain= 'seznam.cz',password='heslo123')
 #session = Session()
-#session.add(ed_user)
+# session.add(ed_user)
 #our_user = session.query(User).filter_by(username='ed').first()
 #ed_user is our_user
 #session.add_all([User(username='wendy', local_part='wendy', domain='gmail.com', password='windy'),User(username='mary', local_part='mary', domain='gmail.com', password='marz123'),User(username='fred', local_part='fred', domain='email.com', password='freddy123')])
-#session.commit()
+# session.commit()
 #for instance in session.query(User).order_by(User.id): print(instance.username,f"{instance.local_part}@{instance.domain}")
